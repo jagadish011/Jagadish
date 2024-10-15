2d6f0b6a283d9922be8ef9634612b0c975bcbc2a
jagadish011 <munavallijagadish@gmail.com>
2024-10-15 13:55:24 +0530

    cahnges in the heroparallex

--- src/components/ui/HeroParallax.jsx
+++ src/components/ui/HeroParallax.jsx
@@ -168,26 +168,21 @@ export const Header = ({ title, description, technologies, links }) => {
       </ul>
       <div className="flex flex-row space-x-4 mt-8">
         {links.map((link, index) => (
-          <a
-            href={link.link}
+          <Link
+            to={link.link}
             target="_blank"
-            rel="noopener noreferrer"
             key={index}
+            className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group"
           >
-            <a
-              href="#_"
-              class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group"
-            >
-              <span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
-              <span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-              <span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-              <span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-              <span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-              <span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease">
-                {link.title}
-              </span>
-            </a>
-          </a>
+            <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+            <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+            <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+            <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+            <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+            <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">
+              {link.title}
+            </span>
+          </Link>
         ))}
       </div>
     </div>
